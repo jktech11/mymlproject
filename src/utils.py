6675f844da37f99c52ad45d4b08bd877eb8658fc
jkhan11 <jktechnology2026@gmail.com>
2026-09-09 12:13:25 +0500

    Extracting and Implementing Model

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from sklearn.metrics import r2_score
 import numpy as np 
 import pandas as pd
 import pickle
+import dill
 
 from src.exception import CustomException
 
@@ -42,6 +43,16 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,para):
 
             return report
 
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_object:
+            obj = pickle.load(file_object)
+        return obj
+    
     except Exception as e:
         raise CustomException(e,sys)
 
